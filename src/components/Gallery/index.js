ac1cd9f627ad8cd9ab710cc2f264c9a87148c15a
Nick Ribal <elektronik2k5@gmail.com>
2018-03-19 23:01:22 +0700

    Add link to route with empty params

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
 import { inject, observer, } from 'mobx-react';
+import {Link} from 'mobx-router';
+import views from '../../config/views';
 import _ from 'lodash';
 
 class Gallery extends Component {
   render() {
+    const {store} = this.props;
     const { queryParams, } = this.props.store.router.snapshot
     return (
       <div>
@@ -12,6 +15,9 @@ class Gallery extends Component {
           {_.map(queryParams, (param, key) => <li key={key}><span>{key}</span> - <b>{param}</b></li>)}
         </ul>
         }
+        <Link view={views.gallery} store={store} queryParams={{}}>
+          Go to gallery, empty queryParams
+        </Link>
       </div>
     );
   }
